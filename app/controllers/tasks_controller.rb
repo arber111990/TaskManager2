da25079d5d7e219b05203ea583ae6c9c5dde5073
Arber Zyb <arber111990@gmail.com>
2019-07-07 10:52:50 +0100

    all controller + routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,30 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-   def show
+  def show
     @tasks = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new    #difference new and create?
+  end
+
+  def create
+    @task = Task.new(params[:id])
+    @restaurant.save
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.update(params[:id])
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/:id", to: "tasks#show"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'photos/:id/update', to: 'task#update'
+  get 'delete', to: 'tasks#destroy'
+  # For details on the DSL available within this file, see
 end
