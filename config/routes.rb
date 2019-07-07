1ca2a3201230c8193785d2861f1419c861a00d7a
Arber Zyb <arber111990@gmail.com>
2019-07-07 18:33:16 +0100

    tried route with resources, still not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/', to: 'tasks#show', as: :task
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id/update', to: 'task#update'
-  get 'tasks/:id/delete', to: 'tasks#destroy'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/', to: 'tasks#show', as: :task
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id/update', to: 'task#update'
+  # get 'tasks/:id/delete', to: 'tasks#destroy'
+
+  resources :tasks                #why it doesn't work?
   # For details on the DSL available within this file, see
 end
